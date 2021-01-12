0db49542fc1ddc628348be85828820cf1db7382e
Karl Keller <kgk86@hotmail.com>
2021-01-12 21:35:11 +0100

    Working on Controlles. TODO: Create the views

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,13 +1,34 @@
 class RestaurantsController < ApplicationController
+  # before_action :find_restaurant, only: [:show]
+
   def index
+    @restaurants = Restaurant.all
   end
 
   def show
+    @restaurant = Restaurant.find(params[:id])
   end
 
   def new
+    @restaurant = Restaurant.new
   end
 
   def create
+    @restaurant = Restaurant.create(set_restaurant)
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def find_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def set_restaurant
+    params.require(:restaurant).permit(:name, :address, :categroy, :reviews)
   end
 end
